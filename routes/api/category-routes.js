b7be835423d0e6dd331dfe3d41a699727d23454e
ericrothmuller <eric@skymarketingco.com>
2022-07-31 17:38:55 -0700

    Added category-routes.js code for get routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,12 +5,36 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  try {
+    const allCategories = await Category.findAll({
   // be sure to include its associated Products
+  include: [{model: Product}],
+});
+res.status(200).json(allCategories);
+} catch (err) {
+console.log(err);
+res.status(400).json(err);
+}
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  try {
+    const singleCategory = await Category.findByPk(req.params.id, {
   // be sure to include its associated Products
+  include: [{model: Product}]
+});
+
+if (!singleCategory) {
+  res.status(404).json({ message: 'Unfortunately, there was no category found.' });
+  return
+}
+
+res.status(200).json(singleCategory);
+} catch (err) {
+console.log(err);
+res.status(400).json(err);
+}
 });
 
 router.post('/', (req, res) => {
