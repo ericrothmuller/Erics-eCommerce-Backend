4214308c613739f28533cf8dce2de31774c8bbac
ericrothmuller <eric@skymarketingco.com>
2022-07-31 18:02:24 -0700

    Added the rest of category-routes.js code: post, put, and delete routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -39,14 +39,55 @@ res.status(400).json(err);
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+    });
+
+    if (updateCategory[0] === 0) {
+      res.status(404).json({ message: 'Unfortunately, there was no category found.' });
+      return
+    }
+    res.status(200).json(updateCategory);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deleteCategory) {
+      res.status(404).json({ message: 'Unfortunately, there was no category found.' });
+      return
+    }
+
+    res.status(200).json(deleteCategory);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
